0a7eaee1f73e2be17e3ca578f6df45a8771bc97e
Oded Green <ogreen@gatech.edu>
2015-11-09 13:39:42 -0500

    starting to collect additional information each iteration
    Conflicts:
    	src/bin/standalone/streaming_partition/main.c

--- src/bin/standalone/streaming_partition/main.c
+++ src/bin/standalone/streaming_partition/main.c
@@ -25,8 +25,6 @@
 #include "stinger_utils/timer.h"
 #include "stinger_core/xmalloc.h"
 
-
-
 #define ACTI(k) (action[2*(k)])
 #define ACTJ(k) (action[2*(k)+1])
 
@@ -49,12 +47,10 @@ static long nbatch = NBATCH_DEFAULT;
 
 static struct stinger * S;
 
-static double * update_time_trace;
-
-
+int64_t compPartitions(struct stinger* GSting, const  int64_t nv, 
+  int64_t* verPartitionBeforeArray, int64_t* verPartitionAfterArray);
 
 void partitionStinger(struct stinger* GSting, const  int64_t nv,const  int64_t numPart, int64_t* verPartitionArray){
-
   int64_t * off;
   int64_t * ind;
   int64_t * weight;
@@ -75,13 +71,13 @@ void partitionStinger(struct stinger* GSting, const  int64_t nv,const  int64_t n
     options[METIS_OPTION_NUMBERING]=0;
     options[METIS_OPTION_NO2HOP]=1;
     idx_t objval;//= (idx_t*)malloc((numPart_)*sizeof(idx_t));
-    idx_t* xadj=off;
-    idx_t* adjncy=ind;
+    idx_t* xadj=(idx_t*)off;
+    idx_t* adjncy=(idx_t*)ind;
 
   //  int temp= METIS_PartGraphRecursive(&nv_, &ncon, xadj, adjncy,  NULL, NULL, NULL, 
   //    &numPart_, NULL, NULL, options, &objval, verPartitionArray);
     int temp= METIS_PartGraphKway(&nv_, &ncon, xadj, adjncy,  NULL, NULL, NULL, 
-      &numPart_, NULL, NULL, options, &objval, verPartitionArray);
+      &numPart_, NULL, NULL, options, &objval, (idx_t*)verPartitionArray);
 
 //    printf("edgecut size: %ld", objval);
 #else
@@ -131,55 +127,18 @@ void partitionStinger(struct stinger* GSting, const  int64_t nv,const  int64_t n
 }
 
 
-void compPartitions(struct stinger* GSting, const  int64_t nv, int64_t* verPartitionBeforeArray,
-    int64_t* verPartitionAfterArray){
 
-    int64_t *countSameAfter= (int64_t*)malloc((nv+1)*sizeof(int64_t));
-    int64_t *countSameBefore= (int64_t*)malloc((nv+1)*sizeof(int64_t));
-    int64_t samePartitions=0;
-
-    MTA("mta assert parallel")
-    MTA("mta block dynamic schedule")
-    OMP("omp parallel for")
-    for(int64_t src=0; src<nv; src++){
-      countSameAfter[src]=0;
-      countSameBefore[src]=0;
-      int edgeCounter=0;
-      STINGER_FORALL_EDGES_OF_VTX_BEGIN(GSting,src)
-        int64_t dest=STINGER_EDGE_DEST;
-
-        if(verPartitionBeforeArray[dest]==verPartitionBeforeArray[src])
-          countSameBefore[src]++;
-        if(verPartitionAfterArray[dest]==verPartitionAfterArray[src])
-          countSameAfter[src]++;
-        edgeCounter++;
-      STINGER_FORALL_EDGES_OF_VTX_END();
-      double percentage=(double)(countSameAfter[src]-countSameBefore[src])/(double)edgeCounter;
-      if(fabs(percentage)<0.3)
-        __sync_add_and_fetch(&samePartitions,1);
-    }
-    free(countSameAfter);
-    free(countSameBefore);
-    PRINT_STAT_INT64("Same partition before and after",samePartitions);
-}
-
-int
-main (const int argc, char *argv[])
-{
+int main (const int argc, char *argv[]){
   parse_args (argc, argv, &initial_graph_name, &action_stream_name, &batch_size, &nbatch);
   STATS_INIT();
-
   load_graph_and_action_stream (initial_graph_name, &nv, &ne, (int64_t**)&off,
 	      (int64_t**)&ind, (int64_t**)&weight, (int64_t**)&graphmem,
 	      action_stream_name, &naction, (int64_t**)&action, (int64_t**)&actionmem);
 
-
-    for(int i=0; i<naction;i++)
-      if(action[2*i]>nv||action[2*i+1]>nv)
-      printf("(%ld , %ld)  , ",action[2*i],action[2*i+1]);
-
   print_initial_graph_stats (nv, ne, batch_size, nbatch, naction);
   BATCH_SIZE_CHECK();
+
+  nbatch=naction/batch_size;
 
 #if defined(_OPENMP)
   OMP("omp parallel") {
@@ -188,43 +147,10 @@ main (const int argc, char *argv[])
   }
 #endif
 
-
-#if METIS==0
-
-  mtmetis_vtx_t *xadj=(mtmetis_vtx_t*)off;
-  mtmetis_adj_t *adjncy=(mtmetis_adj_t*)ind; 
-
-  double* options=mtmetis_init_options();
-
-  options[MTMETIS_OPTION_NPARTS]=2;
-  options[MTMETIS_OPTION_CTYPE]=MTMETIS_CTYPE_SHEM;
-  options[MTMETIS_OPTION_PTYPE]=MTMETIS_PTYPE_ESEP;
-  options[MTMETIS_OPTION_CONTYPE]=MTMETIS_CONTYPE_SORT;
-  options[MTMETIS_OPTION_RTYPE]=MTMETIS_RTYPE_GREEDY;
-  options[MTMETIS_OPTION_NRUNS]=10;
-  options[MTMETIS_OPTION_NITER]=10;
-
-  mtmetis_vtx_t *csrPart= (mtmetis_vtx_t*)malloc(nv*sizeof(mtmetis_vtx_t));
-
-  options[MTMETIS_OPTION_IGNORE]=MTMETIS_IGNORE_VERTEXWEIGHTS+MTMETIS_IGNORE_EDGEWEIGHTS;
-  mtmetis_partition_explicit(
-    nv, 
-    xadj, 
-    adjncy, 
-    NULL, 
-    weight,
-    options, 
-    (mtmetis_pid_t*)csrPart, 
-    NULL);
-
-#endif
-
-  update_time_trace = xmalloc (nbatch * sizeof(*update_time_trace));
-
+  int64_t initialNe=ne, currentNe=ne, lastDoubleNe=ne, doubledFlag=0;
 
   /* Convert to STINGER */
-  tic ();
-  S = stinger_new ();
+  tic (); S = stinger_new ();
   stinger_set_initial_edges (S, nv, 0, off, ind, weight, NULL, NULL, -2);
   PRINT_STAT_DOUBLE ("time_stinger", toc ());
   fflush(stdout);
@@ -237,6 +163,7 @@ main (const int argc, char *argv[])
   PRINT_STAT_HEX64 ("error_code", (long unsigned) errorCode);
   PRINT_STAT_DOUBLE ("time_check", time_check);
 
+<<<<<<< HEAD
   /* Updates */
   int64_t ntrace = 0;
 
@@ -252,81 +179,119 @@ main (const int argc, char *argv[])
   {
     tic();
     int64_t diffPartitions=0;
+=======
+  int64_t *firstPart  = (int64_t*)malloc(nv*sizeof(int64_t));
+  int64_t *currPart   = (int64_t*)malloc(nv*sizeof(int64_t));
+  int64_t *prevPart   = (int64_t*)malloc(nv*sizeof(int64_t));
+  int64_t *lastPart   = (int64_t*)malloc(nv*sizeof(int64_t));
+  int64_t *doublePart = (int64_t*)malloc(nv*sizeof(int64_t));
+
+  int64_t inserted=0,val;
+  int64_t batchId=0;
+
+  partitionStinger(S,nv, 2, firstPart);
+
+  // Going through all the batches
+  for (int64_t actno = 0; actno < nbatch * batch_size; actno += batch_size,batchId++){
+    int64_t diffPartitionsBefore=0,diffPartitionsAfter=0;
+>>>>>>> 0086195... starting to collect additional information each iteration
     const int64_t endact = (((actno + batch_size) > naction) ? naction : actno + batch_size);
     int64_t *actions = &action[2*actno];
     int64_t numActions = endact - actno;
 
-    MTA("mta assert parallel")
-    MTA("mta block dynamic schedule")
+    // Adding the edges into the network 
     OMP("omp parallel for")
     for(uint64_t k = 0; k < endact - actno; k++) {
       const int64_t i = actions[2 * k];
       const int64_t j = actions[2 * k + 1];
         if (i != j && i >= 0) {
+          // Looking for the edges that are cross partition
           if(prevPart[i]!=prevPart[j]){
-            __sync_add_and_fetch(&diffPartitions,1);
+            __sync_add_and_fetch(&diffPartitionsBefore,1);
           }
+<<<<<<< HEAD
       	int val=stinger_insert_edge (S, 0, i, j, 1, actno+2);
+=======
+        // Counting the number of edges inserted into the network for sanity checking
+      	val =stinger_insert_edge (S, 0, i, j, 1, actno+2);
+>>>>>>> 0086195... starting to collect additional information each iteration
       	val+=stinger_insert_edge (S, 0, j, i, 1, actno+2);
-        __sync_add_and_fetch(&inserted,val);
+        __sync_add_and_fetch(&inserted,val); 
       }
     }
+<<<<<<< HEAD
     update_time_trace[ntrace] = toc();
     PRINT_STAT_INT64("Cross partition edges - before :", diffPartitions);
     partitionStinger(S,nv, 2, currPart);
     if(ntrace==0)
       memcpy(firstPart,currPart,nv*sizeof(int64_t));
+=======
+    currentNe+=inserted;
 
-    compPartitions(S,nv, prevPart,currPart);
 
+>>>>>>> 0086195... starting to collect additional information each iteration
+
+    int64_t samePartitions=compPartitions(S,nv, prevPart,currPart);
+    // Swapping partition arrays every iteration;
+    int64_t* temp;temp=prevPart;  prevPart=currPart; currPart=temp;
+    
+
+<<<<<<< HEAD
     temp=prevPart;  prevPart=currPart; currPart=temp;
     ntrace++;
    diffPartitions=0;
     MTA("mta assert parallel")
     MTA("mta block dynamic schedule")
+=======
+    // Graph has doubled the number of edges
+    if (currentNe > (2*lastDoubleNe)){
+      lastDoubleNe=currentNe;
+      doubledFlag=1;
+      memcpy(doublePart,prevPart,nv*sizeof(int64_t));      
+    }
+
+>>>>>>> 0086195... starting to collect additional information each iteration
     OMP("omp parallel for")
     for(uint64_t k = 0; k < endact - actno; k++) {
       const int64_t i = actions[2 * k];
       const int64_t j = actions[2 * k + 1];
         if (i != j && i >= 0) {
           if(prevPart[i]!=prevPart[j]){
-            __sync_add_and_fetch(&diffPartitions,1);
+            __sync_add_and_fetch(&diffPartitionsAfter,1);
           }
       }
     }
-    update_time_trace[ntrace] = toc();
-    PRINT_STAT_INT64("Cross partition edges -  after :", diffPartitions);
+    PRINT_STAT_INT64("Cross partition edges - before :", diffPartitionsBefore);    
+    PRINT_STAT_INT64("Cross partition edges -  after :", diffPartitionsAfter);
+    PRINT_STAT_INT64("Same partition before and after", samePartitions);    
   } /* End of batch */
 
   memcpy(lastPart,prevPart,nv*sizeof(int64_t));
 
   int64_t diffPartitions=0,diffCsrPartitions=0;
 
-  for (int64_t actno = 0; actno < nbatch * batch_size; actno += batch_size)
-  {
-
+  for (int64_t actno = 0; actno < nbatch * batch_size; actno += batch_size)  {
     const int64_t endact = (actno + batch_size > naction ? naction : actno + batch_size);
     int64_t *actions = &action[2*actno];
     int64_t numActions = endact - actno;
 
-    MTA("mta assert parallel")
-    MTA("mta block dynamic schedule")
     OMP("omp parallel for")
     for(uint64_t k = 0; k < endact - actno; k++) {
       const int64_t i = actions[2 * k];
       const int64_t j = actions[2 * k + 1];
-        if (i != j && i >= 0) {
-          if(lastPart[i]!=lastPart[j]){
-            __sync_add_and_fetch(&diffPartitions,1);
-          }
-#if METIS==0
-          if(csrPart[i]!=csrPart[j]){
-            __sync_add_and_fetch(&diffCsrPartitions,1);
-          }
-#endif          
+      if (i != j && i >= 0) {
+        if(lastPart[i]!=lastPart[j]){
+          __sync_add_and_fetch(&diffPartitions,1);
+        }
       }
     }
+    
+  } 
+  /* End of batch */
+  PRINT_STAT_INT64("Actual number of inserted edges :", inserted);
+  PRINT_STAT_INT64("Cross partition for last iteration :", diffPartitions);
 
+<<<<<<< HEAD
   } /* End of batch */
 
     PRINT_STAT_INT64("Actual number of inserted edges :", inserted);
@@ -350,14 +315,49 @@ main (const int argc, char *argv[])
   PRINT_STAT_DOUBLE ("updates_per_sec", (nbatch * batch_size) / time_updates); 
 
   tic ();
-  errorCode = stinger_consistency_check (S, nv);
-  time_check = toc ();
-  PRINT_STAT_HEX64 ("error_code", (long unsigned) errorCode);
-  PRINT_STAT_DOUBLE ("time_check", time_check);
+=======
+  free(firstPart);  free(prevPart);  free(currPart);  free(lastPart); free(doublePart);
 
-  free(update_time_trace);
+>>>>>>> 0086195... starting to collect additional information each iteration
+  errorCode = stinger_consistency_check (S, nv);
+  PRINT_STAT_HEX64 ("error_code", (long unsigned) errorCode);
+
   stinger_free_all (S);
   free (actionmem);
   STATS_END();
 
 }
+
+
+// Given two differnt partitions of the graph this function checks if the vertices stay within 
+// the same partition. 
+int64_t compPartitions(struct stinger* GSting, const  int64_t nv, int64_t* verPartitionBeforeArray,
+    int64_t* verPartitionAfterArray){
+
+    int64_t *countSameAfter= (int64_t*)malloc((nv+1)*sizeof(int64_t));
+    int64_t *countSameBefore= (int64_t*)malloc((nv+1)*sizeof(int64_t));
+    int64_t samePartitions=0;
+
+    OMP("omp parallel for")
+    for(int64_t src=0; src<nv; src++){
+      countSameAfter[src]=0;
+      countSameBefore[src]=0;
+      int edgeCounter=0;
+      STINGER_FORALL_EDGES_OF_VTX_BEGIN(GSting,src)
+        int64_t dest=STINGER_EDGE_DEST;
+
+        if(verPartitionBeforeArray[dest]==verPartitionBeforeArray[src])
+          countSameBefore[src]++;
+        if(verPartitionAfterArray[dest]==verPartitionAfterArray[src])
+          countSameAfter[src]++;
+        edgeCounter++;
+      STINGER_FORALL_EDGES_OF_VTX_END();
+      double percentage=(double)(countSameAfter[src]-countSameBefore[src])/(double)edgeCounter;
+      if(fabs(percentage)<0.3)
+        __sync_add_and_fetch(&samePartitions,1);
+    }
+    free(countSameAfter);
+    free(countSameBefore);
+    return samePartitions;
+}
+
